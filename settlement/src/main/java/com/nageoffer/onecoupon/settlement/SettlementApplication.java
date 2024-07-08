06a612b5fc76ba7dbe7e0b2e2cb71052927429c4
ding.ma <machen@apache.org>
2024-07-08 20:33:41 +0800

    other: 结算服务添加启动类注释以及分配Web访问端口

--- settlement/src/main/java/com/nageoffer/onecoupon/settlement/SettlementApplication.java
+++ settlement/src/main/java/com/nageoffer/onecoupon/settlement/SettlementApplication.java
@@ -37,6 +37,13 @@ package com.nageoffer.onecoupon.settlement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 结算服务｜负责用户下单时订单金额计算功能，因和订单相关联，该服务流量较大
+ * <p>
+ * 作者：马丁
+ * 加星球群：早加入就是优势！500人内部沟通群，分享的知识总有你需要的 <a href="https://t.zsxq.com/cw7b9" />
+ * 开发时间：2024-07-08
+ */
 @SpringBootApplication
 public class SettlementApplication {
 
