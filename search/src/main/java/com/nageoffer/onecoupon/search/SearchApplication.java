6439245524d5a4d3dfe3c858dac9d0289fe4766f
ding.ma <machen@apache.org>
2024-07-08 20:32:16 +0800

    other: 搜素服务添加启动类注释以及分配Web访问端口

--- search/src/main/java/com/nageoffer/onecoupon/search/SearchApplication.java
+++ search/src/main/java/com/nageoffer/onecoupon/search/SearchApplication.java
@@ -37,6 +37,13 @@ package com.nageoffer.onecoupon.search;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 搜索服务｜提供用户优惠券搜索功能
+ * <p>
+ * 作者：马丁
+ * 加星球群：早加入就是优势！500人内部沟通群，分享的知识总有你需要的 <a href="https://t.zsxq.com/cw7b9" />
+ * 开发时间：2024-07-08
+ */
 @SpringBootApplication
 public class SearchApplication {
 
