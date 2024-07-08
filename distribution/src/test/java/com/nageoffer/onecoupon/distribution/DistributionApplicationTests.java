5a204c2792a1e952a4fa21da65015c5c64825fb4
chen.ma <machen@apache.org>
2024-07-08 16:21:08 +0800

    other: 提交代码版权信息和其他变更

--- distribution/src/test/java/com/nageoffer/onecoupon/distribution/DistributionApplicationTests.java
+++ distribution/src/test/java/com/nageoffer/onecoupon/distribution/DistributionApplicationTests.java
@@ -43,5 +43,4 @@ class DistributionApplicationTests {
     @Test
     void contextLoads() {
     }
-
 }
--- settlement/src/test/java/com/nageoffer/onecoupon/settlement/SettlementApplicationTests.java
+++ settlement/src/test/java/com/nageoffer/onecoupon/settlement/SettlementApplicationTests.java
@@ -43,5 +43,4 @@ class SettlementApplicationTests {
     @Test
     void contextLoads() {
     }
-
 }
