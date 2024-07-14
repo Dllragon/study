466e10c27b8fb53b5642af7560507bc06ca0cf09
ding.ma <machen@apache.org>
2024-07-14 18:48:19 +0800

    refactor: 重构优惠券模板信息为Hash，方便后续通过原子执行自减库存逻辑

--- engine/src/main/java/com/nageoffer/onecoupon/engine/service/impl/CouponTemplateServiceImpl.java
+++ engine/src/main/java/com/nageoffer/onecoupon/engine/service/impl/CouponTemplateServiceImpl.java
@@ -35,8 +35,8 @@
 package com.nageoffer.onecoupon.engine.service.impl;
 
 import cn.hutool.core.bean.BeanUtil;
-import cn.hutool.core.util.StrUtil;
-import com.alibaba.fastjson2.JSON;
+import cn.hutool.core.bean.copier.CopyOptions;
+import cn.hutool.core.map.MapUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -51,6 +51,9 @@ import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * 优惠券模板业务逻辑实现层
@@ -73,18 +76,18 @@ public class CouponTemplateServiceImpl extends ServiceImpl<CouponTemplateMapper,
         // TODO 防止缓存穿透
         // 查询 Redis 缓存中是否存在优惠券模板信息
         String couponTemplateCacheKey = String.format(EngineRedisConstant.COUPON_TEMPLATE_KEY, requestParam.getCouponTemplateId());
-        String couponTemplateCache = stringRedisTemplate.opsForValue().get(couponTemplateCacheKey);
+        Map<Object, Object> couponTemplateCacheMap = stringRedisTemplate.opsForHash().entries(couponTemplateCacheKey);
 
         // 如果存在直接返回，不存在需要通过双重判定锁的形式读取数据库中的记录
-        if (StrUtil.isEmpty(couponTemplateCache)) {
+        if (MapUtil.isEmpty(couponTemplateCacheMap)) {
             // 获取优惠券模板分布式锁
             RLock lock = redissonClient.getLock(String.format(EngineRedisConstant.LOCK_COUPON_TEMPLATE_KEY, requestParam.getCouponTemplateId()));
             lock.lock();
 
             try {
                 // 通过双重判定锁优化大量请求无意义查询数据库
-                couponTemplateCache = stringRedisTemplate.opsForValue().get(couponTemplateCacheKey);
-                if (StrUtil.isEmpty(couponTemplateCache)) {
+                couponTemplateCacheMap = stringRedisTemplate.opsForHash().entries(couponTemplateCacheKey);
+                if (MapUtil.isEmpty(couponTemplateCacheMap)) {
                     LambdaQueryWrapper<CouponTemplateDO> queryWrapper = Wrappers.lambdaQuery(CouponTemplateDO.class)
                             .eq(CouponTemplateDO::getShopNumber, Long.parseLong(requestParam.getShopNumber()))
                             .eq(CouponTemplateDO::getId, Long.parseLong(requestParam.getCouponTemplateId()));
@@ -92,9 +95,17 @@ public class CouponTemplateServiceImpl extends ServiceImpl<CouponTemplateMapper,
 
                     if (couponTemplateDO != null) {
                         // 通过将数据库的记录序列化成 JSON 字符串放入 Redis 缓存
-                        String actualCacheValue = JSON.toJSONString(BeanUtil.toBean(couponTemplateDO, CouponTemplateQueryRespDTO.class));
-                        stringRedisTemplate.opsForValue().set(couponTemplateCacheKey, actualCacheValue);
-                        couponTemplateCache = actualCacheValue;
+                        CouponTemplateQueryRespDTO actualRespDTO = BeanUtil.toBean(couponTemplateDO, CouponTemplateQueryRespDTO.class);
+                        Map<String, Object> cacheTargetMap = BeanUtil.beanToMap(actualRespDTO, false, true);
+                        Map<String, String> actualCacheTargetMap = cacheTargetMap.entrySet().stream()
+                                .collect(Collectors.toMap(
+                                        Map.Entry::getKey,
+                                        entry -> entry.getValue() != null ? entry.getValue().toString() : ""
+                                ));
+                        stringRedisTemplate.opsForHash().putAll(couponTemplateCacheKey, actualCacheTargetMap);
+                        couponTemplateCacheMap = cacheTargetMap.entrySet()
+                                .stream()
+                                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
                     }
                 }
             } finally {
@@ -102,6 +113,6 @@ public class CouponTemplateServiceImpl extends ServiceImpl<CouponTemplateMapper,
             }
         }
 
-        return JSON.parseObject(couponTemplateCache, CouponTemplateQueryRespDTO.class);
+        return BeanUtil.mapToBean(couponTemplateCacheMap, CouponTemplateQueryRespDTO.class, false, CopyOptions.create());
     }
 }
