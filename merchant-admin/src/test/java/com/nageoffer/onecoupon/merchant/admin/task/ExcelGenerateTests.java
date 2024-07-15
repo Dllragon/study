9c4fcc94f7eb6bed22cbb2639e5f8438e55888d5
ding.ma <machen@apache.org>
2024-07-15 17:38:33 +0800

    test: 修改Excel默认生成记录条数

--- merchant-admin/src/test/java/com/nageoffer/onecoupon/merchant/admin/task/ExcelGenerateTests.java
+++ merchant-admin/src/test/java/com/nageoffer/onecoupon/merchant/admin/task/ExcelGenerateTests.java
@@ -61,9 +61,9 @@ import java.util.Locale;
 public final class ExcelGenerateTests {
 
     /**
-     * 写入优惠券推送示例 Excel 的数据，默认 20 万行，自行控制即可
+     * 写入优惠券推送示例 Excel 的数据，自行控制即可
      */
-    private final int writeNum = 1000000;
+    private final int writeNum = 5000;
     private final Faker faker = new Faker(Locale.CHINA);
     private final String excelPath = Paths.get("").toAbsolutePath().getParent() + "/tmp";
 
