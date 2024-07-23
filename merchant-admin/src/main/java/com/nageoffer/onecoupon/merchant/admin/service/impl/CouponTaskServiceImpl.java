3be240d89380c39fa5cb557f368b45a7fb2c6bb2
ding.ma <machen@apache.org>
2024-07-23 14:58:22 +0800

    feature: 开发优惠券模板推送任务立即执行功能

--- merchant-admin/src/main/java/com/nageoffer/onecoupon/merchant/admin/service/impl/CouponTaskServiceImpl.java
+++ merchant-admin/src/main/java/com/nageoffer/onecoupon/merchant/admin/service/impl/CouponTaskServiceImpl.java
@@ -132,11 +132,13 @@ public class CouponTaskServiceImpl extends ServiceImpl<CouponTaskMapper, CouponT
         delayedQueue.offer(delayJsonObject, 20, TimeUnit.SECONDS);
 
         // 如果是立即发送任务，直接调用消息队列进行发送流程
-        // 执行优惠券推送业务，正式向用户发放优惠券
-        CouponTaskExecuteEvent couponTaskExecuteEvent = CouponTaskExecuteEvent.builder()
-                .couponTaskId(couponTaskDO.getId())
-                .build();
-        couponTaskActualExecuteProducer.sendMessage(couponTaskExecuteEvent);
+        if (Objects.equals(requestParam.getSendType(), CouponTaskSendTypeEnum.IMMEDIATE.getType())) {
+            // 执行优惠券推送业务，正式向用户发放优惠券
+            CouponTaskExecuteEvent couponTaskExecuteEvent = CouponTaskExecuteEvent.builder()
+                    .couponTaskId(couponTaskDO.getId())
+                    .build();
+            couponTaskActualExecuteProducer.sendMessage(couponTaskExecuteEvent);
+        }
     }
 
     @Override
