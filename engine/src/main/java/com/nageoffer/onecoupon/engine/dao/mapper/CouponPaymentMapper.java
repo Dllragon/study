12a7e4f650583e2e2345f52cd35678cb89726dde
Henry Van <yzwh0102@qq.com>
2024-07-28 00:02:41 +0800

    refactor(service):修改代码结构

--- engine/src/main/java/com/nageoffer/onecoupon/engine/dao/mapper/CouponPaymentMapper.java
+++ engine/src/main/java/com/nageoffer/onecoupon/engine/dao/mapper/CouponPaymentMapper.java
@@ -38,6 +38,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.nageoffer.onecoupon.engine.dao.entity.CunponPaymentDO;
 
 /**
+ * 优惠券支付数据库持久层
  * <p>
  * 作者：Henry Wan
  * 加项目群：早加入就是优势！500人内部项目群，分享的知识总有你需要的 <a href="https://t.zsxq.com/cw7b9" />
