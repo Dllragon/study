90ee45e9e0adcad8806ad8f917a46b3a23476269
ding.ma <machen@apache.org>
2024-08-17 23:50:09 +0800

    feature: 开发基础组件库防重复提交注解运行时支持反射获取功能

--- framework/src/main/java/com/nageoffer/onecoupon/framework/idempotent/NoDuplicateSubmit.java
+++ framework/src/main/java/com/nageoffer/onecoupon/framework/idempotent/NoDuplicateSubmit.java
@@ -35,6 +35,8 @@
 package com.nageoffer.onecoupon.framework.idempotent;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
@@ -45,6 +47,7 @@ import java.lang.annotation.Target;
  * 开发时间：2024-07-10
  */
 @Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface NoDuplicateSubmit {
 
     /**
