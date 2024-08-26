d19f5e7ce71178f38fc06c3be6f315feeb3fa55e
ding.ma <machen@apache.org>
2024-08-26 20:37:35 +0800

    feature: 使用双重判定锁机制优化优惠券模板查询缓存击穿问题

--- engine/src/main/java/com/nageoffer/onecoupon/engine/service/impl/CouponTemplateServiceImpl.java
+++ engine/src/main/java/com/nageoffer/onecoupon/engine/service/impl/CouponTemplateServiceImpl.java
@@ -35,9 +35,12 @@
 package com.nageoffer.onecoupon.engine.service.impl;
 
 import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.bean.copier.CopyOptions;
+import cn.hutool.core.map.MapUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.nageoffer.onecoupon.engine.common.constant.EngineRedisConstant;
 import com.nageoffer.onecoupon.engine.common.enums.CouponTemplateStatusEnum;
 import com.nageoffer.onecoupon.engine.dao.entity.CouponTemplateDO;
 import com.nageoffer.onecoupon.engine.dao.mapper.CouponTemplateMapper;
@@ -46,7 +49,17 @@ import com.nageoffer.onecoupon.engine.dto.resp.CouponTemplateQueryRespDTO;
 import com.nageoffer.onecoupon.engine.service.CouponTemplateService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * 优惠券模板业务逻辑实现层
@@ -61,14 +74,72 @@ import org.springframework.stereotype.Service;
 public class CouponTemplateServiceImpl extends ServiceImpl<CouponTemplateMapper, CouponTemplateDO> implements CouponTemplateService {
 
     private final CouponTemplateMapper couponTemplateMapper;
+    private final StringRedisTemplate stringRedisTemplate;
+    private final RedissonClient redissonClient;
 
     @Override
     public CouponTemplateQueryRespDTO findCouponTemplate(CouponTemplateQueryReqDTO requestParam) {
-        LambdaQueryWrapper<CouponTemplateDO> queryWrapper = Wrappers.lambdaQuery(CouponTemplateDO.class)
-                .eq(CouponTemplateDO::getShopNumber, Long.parseLong(requestParam.getShopNumber()))
-                .eq(CouponTemplateDO::getId, Long.parseLong(requestParam.getCouponTemplateId()))
-                .eq(CouponTemplateDO::getStatus, CouponTemplateStatusEnum.ACTIVE.getStatus());
-        CouponTemplateDO couponTemplateDO = couponTemplateMapper.selectOne(queryWrapper);
-        return BeanUtil.toBean(couponTemplateDO, CouponTemplateQueryRespDTO.class);
+        // 查询 Redis 缓存中是否存在优惠券模板信息
+        String couponTemplateCacheKey = String.format(EngineRedisConstant.COUPON_TEMPLATE_KEY, requestParam.getCouponTemplateId());
+        Map<Object, Object> couponTemplateCacheMap = stringRedisTemplate.opsForHash().entries(couponTemplateCacheKey);
+
+        // 如果存在直接返回，不存在需要通过双重判定锁的形式读取数据库中的记录
+        if (MapUtil.isEmpty(couponTemplateCacheMap)) {
+            // 获取优惠券模板分布式锁
+            // 关于缓存击穿更多注释事项，欢迎查看我的B站视频：https://www.bilibili.com/video/BV1qz421z7vC
+            RLock lock = redissonClient.getLock(String.format(EngineRedisConstant.LOCK_COUPON_TEMPLATE_KEY, requestParam.getCouponTemplateId()));
+            lock.lock();
+
+            try {
+                // 通过双重判定锁优化大量请求无意义查询数据库
+                couponTemplateCacheMap = stringRedisTemplate.opsForHash().entries(couponTemplateCacheKey);
+                if (MapUtil.isEmpty(couponTemplateCacheMap)) {
+                    LambdaQueryWrapper<CouponTemplateDO> queryWrapper = Wrappers.lambdaQuery(CouponTemplateDO.class)
+                            .eq(CouponTemplateDO::getShopNumber, Long.parseLong(requestParam.getShopNumber()))
+                            .eq(CouponTemplateDO::getId, Long.parseLong(requestParam.getCouponTemplateId()))
+                            .eq(CouponTemplateDO::getStatus, CouponTemplateStatusEnum.ACTIVE.getStatus());
+                    CouponTemplateDO couponTemplateDO = couponTemplateMapper.selectOne(queryWrapper);
+
+                    if (couponTemplateDO != null) {
+                        // 通过将数据库的记录序列化成 JSON 字符串放入 Redis 缓存
+                        CouponTemplateQueryRespDTO actualRespDTO = BeanUtil.toBean(couponTemplateDO, CouponTemplateQueryRespDTO.class);
+                        Map<String, Object> cacheTargetMap = BeanUtil.beanToMap(actualRespDTO, false, true);
+                        Map<String, String> actualCacheTargetMap = cacheTargetMap.entrySet().stream()
+                                .collect(Collectors.toMap(
+                                        Map.Entry::getKey,
+                                        entry -> entry.getValue() != null ? entry.getValue().toString() : ""
+                                ));
+
+                        // 通过 LUA 脚本执行设置 Hash 数据以及设置过期时间
+                        String luaScript = "redis.call('HMSET', KEYS[1], unpack(ARGV, 1, #ARGV - 1)) " +
+                                "redis.call('EXPIREAT', KEYS[1], ARGV[#ARGV])";
+
+                        List<String> keys = Collections.singletonList(couponTemplateCacheKey);
+                        List<String> args = new ArrayList<>(actualCacheTargetMap.size() * 2 + 1);
+                        actualCacheTargetMap.forEach((key, value) -> {
+                            args.add(key);
+                            args.add(value);
+                        });
+
+                        // 优惠券活动过期时间转换为秒级别的 Unix 时间戳
+                        args.add(String.valueOf(couponTemplateDO.getValidEndTime().getTime() / 1000));
+
+                        // 执行 LUA 脚本
+                        stringRedisTemplate.execute(
+                                new DefaultRedisScript<>(luaScript, Long.class),
+                                keys,
+                                args.toArray()
+                        );
+                        couponTemplateCacheMap = cacheTargetMap.entrySet()
+                                .stream()
+                                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+                    }
+                }
+            } finally {
+                lock.unlock();
+            }
+        }
+
+        return BeanUtil.mapToBean(couponTemplateCacheMap, CouponTemplateQueryRespDTO.class, false, CopyOptions.create());
     }
 }
